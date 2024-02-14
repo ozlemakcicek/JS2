27aa650c257e431d100bab103c8b8e0ff36e4a71
ozlemakcicek <ozlemakcicek83@gmail.com>
2024-02-14 12:57:11 +0100

    comment added

--- class-notes/DE-19-Module/modules/index.js
+++ class-notes/DE-19-Module/modules/index.js
@@ -1,5 +1,5 @@
 
-// import ederkenexport sayfasindaki butun sayfayi gonderme fonksiyonunun onune import yaz sonra degiskenin adi sonra , ve {} icine kimi cagiracaksan yaz ve from"" icine export sayfasinin adresi
+// import ederken export sayfasindaki butun sayfayi gonderme fonksiyonunun onune import yaz sonra degiskenin adi sonra , ve {} icine kimi cagiracaksan yaz ve from"" icine export sayfasinin adresi
 
 import ugurla,{topla,CALISMA_SAATI,inc,dec,benimAdim} from"./myModule.js"
 
--- class-notes/DE-19-Module/modules/myModule.js
+++ class-notes/DE-19-Module/modules/myModule.js
@@ -1,9 +1,12 @@
+//! JS de export-import olayi
+
 // ya herbirinin basina tek tek export yazilir.ya da cagirmak istediklerimizi tek bir export{} icine yazariz.Ya da butun bir sayfayi export etmek icin 1 tane sectigimiz bir fonksiyon onune export default yazariz.3 yontem var export ederken
 
+// burda export edip index.js de import edelim.{} icinde karsilanir
 
 console.log("module den selamlar");
 
-export const CALISMA_SAATI=40
+export const CALISMA_SAATI=40  
 
 export function topla(s1,s2){
     return s1+s2
@@ -23,12 +26,12 @@ console.log(counter);
 }
 
 
-const benimAdim="Ashley"
+const benimAdim="Ashley"  
 
 
-export{inc,dec,benimAdim}
+export{inc,dec,benimAdim}  // boyle paketle gonderince karsilarken de {} icinde paket olarak alir.ustte export ve export function ile gonderdiklerimizi de {} icinde karsilariz
 
 //? 1 sayfada sadece 1kez export default yazilablr
-export default function ugurla(){
+export default function ugurla(){   // bunu direkt export defaiult oile gonderdigimiz icin karsilarken acik bi sekilde yazariz
     console.log("Elvedda JS hosgeldin React");
 }
